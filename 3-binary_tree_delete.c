52d53d254d84dbad3ce9fd63df71a966f6449407
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-26 23:41:21 +0100

    1-4

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - func to delete a whole tree
+ * @tree: this is supposed to be the top of the tree
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
