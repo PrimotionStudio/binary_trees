ff21c7ed11fd380bcf238c65f9f9a7ffd4e158d1
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-26 23:34:58 +0100

    1-4

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Function to create a new node
+ * at the left of the parent
+ * @parent: the parent node of this new node
+ * @value: the value associated with this new node
+ * Return: the newNode created
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		newNode->left->parent = newNode;
+	}
+	parent->left = newNode;
+	newNode->parent = parent;
+	return (newNode);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Function to create a new node
+ * at the right of the parent
+ * @parent: the parent node of this new node
+ * @value: the value associated with this new node
+ * Return: the newNode created
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	if (parent->right != NULL)
+	{
+		newNode->right = parent->right;
+		newNode->right->parent = newNode;
+	}
+	parent->right = newNode;
+	newNode->parent = parent;
+	return (newNode);
+}
