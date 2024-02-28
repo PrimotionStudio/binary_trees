130b5bdc7c7e064da490a7e583c802c9b298f18e
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-28 22:36:49 +0100

    0-17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - a func to get the siblibg of a node
+ * @node: the node
+ * Return: the height of the tree
+ */
+binary_tree_t binary_tree_sibling(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
