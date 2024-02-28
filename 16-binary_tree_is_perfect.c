c333c1897bba650b88c99c1cfa838f501e77aeb2
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-28 22:27:54 +0100

    0-16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,16 +1,46 @@
 #include "binary_trees.h"
 
 /**
+ * traverse_h- a function to traverse the tree
+ * @root: the root of the func
+ * @i: a counter
+ */
+void traverse_h(const binary_tree_t *root, size_t *i)
+{
+	size_t j = 0, k = 0;
+
+	if (root == NULL)
+		return;
+	if (root->left != NULL || root->right != NULL)
+	{
+		traverse_h(root->left, &j);
+		traverse_h(root->right, &k);
+		if (j > k)
+			*i = j + 1;
+		else
+			*i = k + 1;
+	}
+}
+
+/**
  * traverse - a function to traverse the tree
  * @root: the root of the func
  * @i: a counter
  */
 void traverse(const binary_tree_t *root, int *i)
 {
+	size_t y = 0, z = 0;
 	if (root == NULL)
 		return;
 	if ((root->left != NULL && root->right == NULL) ||
 			(root->left == NULL && root->right != NULL))
+	{
+		*i = 0;
+		return;
+	}
+	traverse_h(root->left, &y);
+	traverse_h(root->right, &z);
+	if (y != z)
 	{
 		*i = 0;
 		return;
