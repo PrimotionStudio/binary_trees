a89b798e32ac53a47a68ed20dde6a7cf9070c640
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-28 04:59:30 +0100

    0-10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	const binary_tree_t *temp = tree;
 	size_t i = 0;
 
-	while (temp->parent != NULL)
+	while (tree != NULL && temp->parent != NULL)
 	{
 		i++;
 		temp = temp->parent;
