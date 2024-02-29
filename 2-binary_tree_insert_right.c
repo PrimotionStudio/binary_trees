22e9c2c4961a7e7b66672b604e16f9885762d231
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-29 21:30:15 +0100

    0-18

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,19 +9,16 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode;
 
-	if (newNode == NULL)
+	if (parent == NULL)
 		return (NULL);
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
+	newNode = binary_tree_node(parent, value);
 	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
 		newNode->right->parent = newNode;
 	}
 	parent->right = newNode;
-	newNode->parent = parent;
 	return (newNode);
 }
